38a9bb55b2daa3f66b04c9fd86063a70a80ba73c
Ashish Singh <assingh@redhat.com>
2021-12-09 04:01:03 -0500

    Demo: Chnage the nearfull ratio to .70
    Signed-off-by: Ashish Singh <assingh@redhat.com>

--- controllers/storagecluster/reconcile.go
+++ controllers/storagecluster/reconcile.go
@@ -43,7 +43,7 @@ const (
 bdev_flock_retry = 20
 mon_osd_full_ratio = .85
 mon_osd_backfillfull_ratio = .8
-mon_osd_nearfull_ratio = .75
+mon_osd_nearfull_ratio = .70
 mon_max_pg_per_osd = 600
 mon_pg_warn_max_object_skew = 0
 [osd]
